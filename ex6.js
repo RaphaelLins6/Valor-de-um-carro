b8e6c1eaf209ea45af9439355596c7c0bf8f55c1
Raphael Lins <rapharell06@gmail.com>
2022-09-15 19:51:26 -0300

    Add files via upload

---
+++ ex6.js
@@ -0,0 +1,5 @@
+let custconcei = Number(prompt('Digite o valor de fábrica: '))
+let percdist = custconcei * 0.45
+let percimp  = custconcei * 0.28
+let custfinal = custconcei + percdist + percimp
+alert('O valor para o consumidor é de: ' + custfinal + ' reais.')
